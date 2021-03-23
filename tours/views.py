bb778a966bcad64cbf9a45e85d2680def860355a
Dmitry <PanshinDA@yandex.ru>
2021-03-24 00:04:37 +0500

    Правки

--- tours/views.py
+++ tours/views.py
@@ -3,40 +3,44 @@ import random
 from django.http import Http404, HttpResponseNotFound, HttpResponseServerError
 from django.shortcuts import render
 
-import tours.data as data
+import tours.data as tours_data
 
 
 def main_view(request):
     """ Главная """
 
     # перемешиваем доступные ключи словарей
-    tours_random = list(data.tours.keys())
+    tours_random = list(tours_data.tours.keys())
     random.shuffle(tours_random)
 
     # формируем список словарей
     tours_dict = dict()
     for number in tours_random[:6]:
-        tours_dict[number] = data.tours[number]
+        tours_dict[number] = tours_data.tours[number]
 
-    about = {'title': data.title,
-             'subtitle': data.subtitle,
-             'description': data.description}
+    about = {
+        'title': tours_data.title,
+        'subtitle': tours_data.subtitle,
+        'description': tours_data.description
+    }
 
-    context = {'menu_header': data.departures,
-               'about': about,
-               'tours': tours_dict}
+    context = {
+        'menu_header': tours_data.departures,
+        'about': about,
+        'tours': tours_dict
+    }
 
     return render(request, 'tours/index.html', context=context)
 
 
 def departure_view(request, departure):
     """ Направления """
-    if departure not in data.departures.keys():
+    if departure not in tours_data.departures.keys():
         raise Http404
 
     tours_dict, price, night = dict(), [], []
-    for num, tour in data.tours.items():
-        if data.tours[num]['departure'] == departure:
+    for num, tour in tours_data.tours.items():
+        if tours_data.tours[num]['departure'] == departure:
             tours_dict[num] = tour
 
             # прайс и ночи, от и до
@@ -46,35 +50,40 @@ def departure_view(request, departure):
     tours_count = len(tours_dict)
 
     # информация о найденых турах
-    fly_from = data.departures[departure]
-    print(fly_from)
+    fly_from = tours_data.departures[departure]
 
-    info = {'count': tours_count,
-            'price_min': min(price),
-            'price_max': max(price),
-            'night_min': min(night),
-            'night_max': max(night)}
+    info = {
+        'tours_count': tours_count,
+        'price_min': min(price),
+        'price_max': max(price),
+        'night_min': min(night),
+        'night_max': max(night)
+    }
 
-    context = {'menu_header': data.departures,
-               'departures': tours_dict,
-               'info': info,
-               'fly_from': fly_from}
+    context = {
+        'menu_header': tours_data.departures,
+        'departures': tours_dict,
+        'info': info,
+        'fly_from': fly_from
+    }
 
     return render(request, 'tours/departure.html', context=context)
 
 
 def tour_view(request, id_tour):
     """ Тур """
-    tour = data.tours.get(id_tour, False) or False
+    tour = tours_data.tours.get(id_tour, False) or False
 
     if not tour:
         raise Http404
 
-    tour_to_where = data.departures[tour['departure']]
+    tour_to_where = tours_data.departures[tour['departure']]
 
-    context = {'menu_header': data.departures,
-               'tour': tour,
-               'tour_to_where': tour_to_where}
+    context = {
+        'menu_header': tours_data.departures,
+        'tour': tour,
+        'tour_to_where': tour_to_where
+    }
 
     return render(request, 'tours/tour.html', context=context)
 
